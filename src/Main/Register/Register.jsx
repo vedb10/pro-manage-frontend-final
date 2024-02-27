697ef9e9419bac5a6412aa0a8e08f6ef2c555dd2
Ved Bhosle <80746745+vedb10@users.noreply.github.com>
2024-02-27 17:15:44 +0530

    Update Register.jsx

--- src/Main/Register/Register.jsx
+++ src/Main/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, {  useState,useNavigate } from 'react'
+import React, {  useState } from 'react'
 import { Link } from "react-router-dom";
 import loginImg from '../../assets/loginImg.png'
 import styles from './register.module.css'
@@ -11,7 +11,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import { registerUser } from '../../apis/auth';
 
 export default function Register() {
-  const navigate = useNavigate()
+  
   const  [submitCheck, setSubmitCheck] = useState(false)
   const [eye, setEye] = useState("password")
   const [formData, setFormData] = useState({
@@ -40,7 +40,7 @@ export default function Register() {
         });
 
         const response = await registerUser({...formData})
-        navigate('/')
+        setSubmitCheck(false)
         
       
       }  else toast.error('Password did not match', {
